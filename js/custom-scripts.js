de80b53d5da1ea0e2a51a603ff84036941ba08e4
bIropka <biropka@gmail.com>
2017-04-13 22:33:33 +0300

    final

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -44,4 +44,47 @@ $(document).ready(function() {
 
     });
 
+    /* universities */
+
+    $('.form-chooser .custom-select').click(function() {
+
+        if($(this).hasClass('active')) {
+            $('.form-chooser .custom-select.active').removeClass('active');
+        } else {
+            $('.form-chooser .custom-select.active').removeClass('active');
+            $(this).addClass('active');
+        }
+
+    });
+
+    $('.form-chooser .custom-select ul li').click(function() {
+
+        var current = $(this).text();
+        $(this).text($(this).parents('.custom-select').find('.current-value').text());
+        $(this).parents('.custom-select').find('.current-value').text(current);
+        $(this).parents('.custom-select').find('input').attr('value', current);
+
+    });
+
+    $('.university-specialities .see-all').click(function() {
+        $(this).toggleClass('active');
+        $(this).siblings('.hidden').fadeToggle(300);
+    });
+
+    if($(window).width() < 481) {
+        $('.university-specialities .speciality:nth-of-type(3)').addClass('hidden');
+    } else {
+        $('.university-specialities .speciality:nth-of-type(3)').removeClass('hidden');
+    }
+
+    $(window).resize(function() {
+
+        if($(window).width() < 481) {
+            $('.university-specialities .speciality:nth-of-type(3)').addClass('hidden');
+        } else {
+            $('.university-specialities .speciality:nth-of-type(3)').removeClass('hidden');
+        }
+
+    });
+
 });
